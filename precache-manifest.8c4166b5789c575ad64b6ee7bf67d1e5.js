3e0d9ccf3807f4cd8c2a5bba228b2f840cf93c20
gustavotempesta <gustavotempesta@users.noreply.github.com>
2022-02-23 19:50:53 +0000

    deploy: 9b1225626450abd995fd9ef18884577d22d4c426

---
+++ precache-manifest.8c4166b5789c575ad64b6ee7bf67d1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fcb1e0a0166365e5da1e78ba7e91e8",
+    "url": "/pokedex-3/index.html"
+  },
+  {
+    "revision": "25f399e42730db01a24d",
+    "url": "/pokedex-3/static/js/2.91516d71.chunk.js"
+  },
+  {
+    "revision": "33719ed1a4fc7a591565",
+    "url": "/pokedex-3/static/js/main.8adee09b.chunk.js"
+  },
+  {
+    "revision": "51c33dad73c906eab431",
+    "url": "/pokedex-3/static/js/runtime~main.75040563.js"
+  },
+  {
+    "revision": "ef2decb1b3cfe0c9e0f83ee37b961b21",
+    "url": "/pokedex-3/static/media/coracao.ef2decb1.svg"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokedex-3/static/media/logo.3cfd5e85.svg"
+  }
+]);
