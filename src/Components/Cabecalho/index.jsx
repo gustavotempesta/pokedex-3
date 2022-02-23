9b1225626450abd995fd9ef18884577d22d4c426
Gustavo Lorenzon Tempesta <gustavo.tempesta@vr.com.br>
2022-02-23 16:39:18 -0300

    Estilização - Meus Favoritos

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -3,23 +3,41 @@ import logo from "../../assets/img/logo.svg";
 import styled from "styled-components";
 
 const Header = styled.header`
-    padding: 27px 0 0 ;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     width: 870px;
     margin: 0 auto;
 `;
 
+const Menu = styled.div`
+    margin-top: 10px;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 17px;
+    color: #3763AD;
+    width: 150px;
+`;
+
+const Favoritos = styled.a`
+    text-decoration: underline;
+    font-size: 15px;
+`;
+
 const Logo = styled.img`
+    margin: 27px 0 0 ;
     width: 186.39px;
     height: 66.87px;
-
 `;
 
 function Cabecalho () {
     return ( 
         <Header>
+            <Menu></Menu>
             <Logo src={logo} alt="Logo Pokémon"/>
+            <Menu>
+            ❤ <Favoritos>Meus Favoritos</Favoritos>
+            </Menu>
         </Header>
      );
 };
